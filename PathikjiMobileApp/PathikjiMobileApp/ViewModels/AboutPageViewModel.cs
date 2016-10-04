812522a896eb8896cea7eaf80a262eb7ec4c33ef
sushimmehta <sushim.git@mailnull.com>
2016-10-04 10:53:00 +0530

    Added the about page

--- PathikjiMobileApp/PathikjiMobileApp/ViewModels/AboutPageViewModel.cs
+++ PathikjiMobileApp/PathikjiMobileApp/ViewModels/AboutPageViewModel.cs
@@ -8,9 +8,20 @@ namespace PathikjiMobileApp.ViewModels
 {
     public class AboutPageViewModel : BindableBase
     {
+        public string AboutHeading { get; private set; }
+
+        public string AboutText { get; private set; }
+
         public AboutPageViewModel()
         {
+            AboutHeading =
+                "Sadhu vesh mein PATHIK";
 
+            AboutText =
+                "Sant Pathikji Maharaj was born in a kanyakubj-brahmin family. " +
+                "He spent his childhood with his maternal grand parents. " +
+                "There he acquired primary education. " +
+                "From the beginning, he had faith and reverence for local gods and godesses.";
         }
     }
 }
