393f2002469e8fa6706e4b7fb9c783f27a2f4ab6
sushimmehta <sushim.git@mailnull.com>
2016-10-04 09:47:00 +0530

    Set package name, application name, version name and version number

--- PathikjiMobileApp/PathikjiMobileApp.Droid/MainActivity.cs
+++ PathikjiMobileApp/PathikjiMobileApp.Droid/MainActivity.cs
@@ -11,7 +11,7 @@ using Microsoft.Practices.Unity;
 
 namespace PathikjiMobileApp.Droid
 {
-    [Activity(Label = "PathikjiMobileApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Pathikji", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
